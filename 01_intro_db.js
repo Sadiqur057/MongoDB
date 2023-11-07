1f3ac0e4a8245c01483da6c98da6eb6ce7e38d0a
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-11-07 20:04:13 +0600

    show,use,drop db & collections

---
+++ 01_intro_db.js
@@ -0,0 +1,14 @@
+// Show all the databse
+show dbs
+
+// Create/ Switched to database
+use university
+
+// Drop database 
+db.dropDatabase()
+
+// Create collection/table
+db.createCollection("students")
+
+// Drop collection
+db.students.drop()
